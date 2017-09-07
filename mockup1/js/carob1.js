d9af1f637ce5f90efddec079d79ee9935d0c88d2
Igor Entaltsev <igor.yentaltsev@gmail.com>
2017-09-07 13:31:17 -0400

    More changes

--- mockup1/js/carob1.js
+++ mockup1/js/carob1.js
@@ -1,4 +1,4 @@
-$( document ).ready(function() {
+jQuery( document ).ready(function() {
     console.log( "ready!" );
 
     var show_david = function( thisObj) {
@@ -11,37 +11,37 @@ $( document ).ready(function() {
     	thisObj.siblings(".donald").delay(500).show( "fade", 700);
     }
 
-    $(".betty").on( "mouseenter", function() {
-    	// console.log( $( this ).find("img").attr("src"));
-    	// console.log( $( this ).siblings(".brian").find(".daniel").text());
+    jQuery(".betty").bind( "mouseenter", function() {
+    	// console.log( jQuery( this ).find("img").attr("src"));
+    	// console.log( jQuery( this ).siblings(".brian").find(".daniel").text());
 
     	var options = { };
-    	var options = { easing: "swing"};
+    	// var options = { easing: "swing"};
     	// var options = {direction: "left"};
 
 
 
-    	// $( this ).siblings(".brian").toggleClass("flex-on");
-    	$( this ).children(".david").show( "slide", options, 700); // v1
-    	// $( this ).siblings(".donald").hide(show_david( $(this)), 100); // v2
+    	// jQuery( this ).siblings(".brian").toggleClass("flex-on");
+    	jQuery( this ).children(".david").show( "slide", options, 700); // v1
+    	// jQuery( this ).siblings(".donald").hide(show_david( jQuery(this)), 100); // v2
     	// .find(".brian")
     	//.show( "slide", {direction: "right"}, 1000);
     	
     });
 
-    $(".betty").on( "mouseleave", function() {
-    	// console.log( $( this ).find("img").attr("src"));
-    	// console.log( $( this ).siblings(".brian").find(".daniel").text());
+    jQuery(".betty").bind( "mouseleave", function() {
+    	// console.log( jQuery( this ).find("img").attr("src"));
+    	// console.log( jQuery( this ).siblings(".brian").find(".daniel").text());
 
     	var options = {};
 
     	// var options = {direction: "left"};
 
     	
-    	// $( this ).siblings(".brian").toggleClass("flex-on");
-    	$( this ).children(".david").hide( "fade", options, 100); // v1
-    	// $( this ).children(".david").hide( "fade", show_donald( $(this)), 100); // v2
-    	// $( this ).children(".david").hide("fade", options, 1000, show_donald( $( this )));
+    	// jQuery( this ).siblings(".brian").toggleClass("flex-on");
+    	jQuery( this ).children(".david").hide( "fade", options, 100); // v1
+    	// jQuery( this ).children(".david").hide( "fade", show_donald( jQuery(this)), 100); // v2
+    	// jQuery( this ).children(".david").hide("fade", options, 1000, show_donald( jQuery( this )));
     	// .find(".brian")
     	//.show( "slide", {direction: "right"}, 1000);
 
